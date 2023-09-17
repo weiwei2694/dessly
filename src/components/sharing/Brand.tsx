12c96cfdcfe250cdea8d4aa443ae9b691beee1f3
Arya Priema <99aryaprima@gmail.com>
2023-09-17 08:40:03 +0700

    fix: Brand
    key

--- src/components/sharing/Brand.tsx
+++ src/components/sharing/Brand.tsx
@@ -6,9 +6,8 @@ const Brand = () => {
         <div className="py-5 lg:py-10">
             <div className="flex items-center justify-evenly sm:justify-between flex-wrap gap-x-4 space-y-4">
                 {brands.map(brand => (
-                    <div className="relative w-[224px] h-[121px]">
+                    <div key={brand.alt} className="relative w-[224px] h-[121px]">
                         <Image
-                            key={brand.alt}
                             src={brand.src}
                             alt={brand.alt}
                             fill
